54dc5b916a0e12c579d376f84a45117232adf891
Matheus Gustavo <matheus_mgp@hotmail.com>
2023-09-20 08:25:00 -0300

    FEAT: NotFoundRoutes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import { PayableRoutes } from './payable.route';
 import { TransactionRoutes } from './transaction.route';
+import { NotFoundRoutes } from './not-found.route';
 
 const router = Router();
 
 router.use(new PayableRoutes().router);
 router.use(new TransactionRoutes().router);
+router.use(new NotFoundRoutes().router);
 
 export { router };
